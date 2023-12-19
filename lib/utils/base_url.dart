6d5f79b8050f735e2d7e419b0b2a21bf5589c4c6
Muhammad Rhomaedi <57169929+Rhomaedy1201@users.noreply.github.com>
2023-12-19 21:51:44 +0700

    Update base_url.dart

--- lib/utils/base_url.dart
+++ lib/utils/base_url.dart
@@ -1,3 +1,4 @@
 String urlWeb = 'https://ecommerce.wstif3c.id';
 // String urlWeb = 'https://2ac1-125-166-119-114.ap.ngrok.io';
+// 
 String baseUrl = '$urlWeb/go-rent-json-api/api'; // belakang url gausah /
